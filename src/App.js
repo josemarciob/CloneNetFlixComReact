5cc7c3bfae4ebbd5c82910b94771988d225dfddc
José Marcio <jjmarcio159@gmail.com>
2021-09-01 15:41:58 -0300

    Exibindo as imagens de backdrop para os filmes em destaques com gradientes lineares transparentes.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Tmdb from './Tmdb'
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
 export default () => {
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(() => {
 
@@ -12,6 +14,15 @@ export default () => {
       //Pegando a lista total
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      //pegando o Featured
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+
+      setFeaturedData(chosenInfo);
+
     }
 
     loadAll();
@@ -20,6 +31,11 @@ export default () => {
 
   return (
     <div className="page">
+
+    {featuredData &&
+      <FeaturedMovie item={featuredData} />
+    }
+
       <section className="lists">
           {movieList.map((item, key) => (
             <MovieRow key={key} title={item.title} items={item.items} />
---
+++ src/components/FeaturedMovie.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './FeaturedMovie.css';
+
+export default ({item}) => {
+  return (
+    <section className="featured" style={{
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+    }}>
+      <div className="featured--vertical">
+        <div className="featured--horizontal">
+          <div className="featured--name">{item.original_name}</div>
+        </div>
+      </div>
+    </section>
+  );
+}
