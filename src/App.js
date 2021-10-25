ffdbace87166d1a99732392372afc1b7ab40d389
José Marcio <jjmarcio159@gmail.com>
2021-10-25 17:03:02 -0300

    Adicionando e pegando as informações do filme escolhido para destaque na tela (Nome, pontuação/relevância, ano, quantidade de temporada(s), descrição, botões(assistir, adicionar a sua lista), gêneros) e o visual

--- src/App.js
+++ src/App.js
@@ -1,46 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 import Tmdb from './Tmdb'
-import MovieRow from './components/MovieRow';
-import FeaturedMovie from './components/FeaturedMovie';
+import MovieRow from './components/MovieRow'
+import FeaturedMovie from './components/FeaturedMovie'
 
 export default () => {
-  const [movieList, setMovieList] = useState([]);
-  const [featuredData, setFeaturedData] = useState(null);
+  const [movieList, setMovieList] = useState([])
+  const [featuredData, setFeaturedData] = useState(null)
 
   useEffect(() => {
-
     const loadAll = async () => {
       //Pegando a lista total
-      let list = await Tmdb.getHomeList();
-      setMovieList(list);
-
+      let list = await Tmdb.getHomeList()
+      setMovieList(list)
       //pegando o Featured
-      let originals = list.filter(i=>i.slug === 'originals');
-      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
-      let chosen = originals[0].items.results[randomChosen];
-      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      let originals = list.filter(i => i.slug === 'originals')
+      //Escolha aleatória
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      )
+      //Filme escolhido
+      let chosen = originals[0].items.results[randomChosen]
+      //Pegando a Info do filme escolhido
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
 
-      setFeaturedData(chosenInfo);
-
+      setFeaturedData(chosenInfo)
     }
-
-    loadAll();
-
+    loadAll()
   }, [])
 
   return (
     <div className="page">
-
-    {featuredData &&
-      <FeaturedMovie item={featuredData} />
-    }
-
+      {featuredData && <FeaturedMovie item={featuredData} />}
       <section className="lists">
-          {movieList.map((item, key) => (
-            <MovieRow key={key} title={item.title} items={item.items} />
-          ))}
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
       </section>
     </div>
-  );
+  )
 }
